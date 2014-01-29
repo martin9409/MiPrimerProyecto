3abc73aeded981b27757c2dc4d131d1602d8c7cb
Alumno <alumno@localhost.localdomain>
2014-01-29 08:34:49 -0600

    loquesea

---
+++ src/AimlessGhost.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class AimlessGhost
+{
+	public static void main(String[] args)
+	{ 
+		Scanner mi_scanner=new Scanner (System.in);
+		// TODO Auto-generated method stub
+		int mi_variable= mi_scanner.nextInt();
+		if (mi_variable == 10)
+		{
+			System.out.print("es un 10");
+		}
+		else{
+			System.out.print("no es un 10");
+		}
+	}
+		
+}	
+
+
